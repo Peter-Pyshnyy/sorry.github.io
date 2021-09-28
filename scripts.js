99ae4c9ec675e6157a6cd8c53d32c74c4c973487
Peter Pyshnyy <petyapyshnyy@gmail.com>
2021-09-28 15:16:35 +0200

    added comments

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 let audio = new Audio("mediafiles/heh.mp3");
 let curtain = document.getElementById("curtain");
 document.addEventListener("mousemove", (e) => {
+  //distance between mouse and the dot (Pythagoras)
   let a =
     Math.sqrt(Math.pow(e.clientY - 302, 2) + Math.pow(e.clientX - 302, 2)) *
-    0.003;
+    0.003; //*0.003 fo fading in-out
+
   curtain.style.background = `rgb(0,0,0,${a})`;
   if (a < 0.03) audio.play();
-  console.log(a);
 });
